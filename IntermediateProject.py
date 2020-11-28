805c8b9ee3c3b66a7f652e57bfac35b691adea46
ArcanionDev <info.arcanion@gmail.com>
2020-11-28 18:21:08 +0100

    Main commit
    first commit, tutorial A star viz in Python

---
+++ IntermediateProject.py
@@ -0,0 +1,14 @@
+import math
+
+print(math)
+
+# code for grid
+
+width = int(input("how wide "))
+height = int(input("how tall "))
+grid = []
+i = int(0)
+for i in range(width):
+    grid.append("_")
+for i in range(height):
+    print(grid)
